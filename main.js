0c5cfb2f4a1920739c91756a8589e28e77faca58
azlanajju <muhammedazlan11@gmail.com>
2023-08-20 16:34:11 +0530

    first commit

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 function copyText(elementId, copiedElementId) {
-  var copyText = document.getElementById(elementId);
-  copyText.select(); 
-  navigator.clipboard.writeText(copyText.value);
-  
-  var copiedElement = document.getElementById(copiedElementId);
-  copiedElement.innerHTML = "Copied!";
+  var copyText = document.getElementById(elementId).textContent;
+  navigator.clipboard.writeText(copyText)
+    .then(() => {
+      var copiedElement = document.getElementById(copiedElementId);
+      copiedElement.innerHTML = "Copied!";
+    })
+    .catch((error) => {
+      console.error('Copy failed: ', error);
+    });
 }
-
-
-
-
-
